4f4d2f53286bdf95bd491f03de43702b7695fd73
kwaterman32-ux <kwaterman32@bentleyschool.org>
2025-11-19 17:05:17 -0800

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Student Monitoring System</h1>
+      <p>Connected and Ready</p>
+    </div>
+  );
+}
+
+export default App;
